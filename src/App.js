b6357293e91185d9bfd27a28f00a902d21a20ef9
Boa Matule <boa.matule@gmail.com>
2019-07-01 01:17:20 +0200

    Data input fields for the User and Computer score.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ class App extends Component {
   render() {
     return (
       <div>
+        <div>
+          <label>userScore</label>
+          <input name="userScore" />
+        </div>
 
+        <div>
+          <label>compScore</label>
+          <input name="compScore" />
+        </div>
       </div>
     );
   }
